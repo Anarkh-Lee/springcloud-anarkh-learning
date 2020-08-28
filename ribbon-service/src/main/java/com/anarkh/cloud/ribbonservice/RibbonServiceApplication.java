aaf463f08574889cd9aeff9d9a8ba71fa1d58e53
Anarkh-Lee <375934935@qq.com>
2020-08-28 20:17:59 +0800

    mod

--- ribbon-service/src/main/java/com/anarkh/cloud/ribbonservice/RibbonServiceApplication.java
+++ ribbon-service/src/main/java/com/anarkh/cloud/ribbonservice/RibbonServiceApplication.java
@@ -2,8 +2,9 @@ package com.anarkh.cloud.ribbonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class RibbonServiceApplication {
